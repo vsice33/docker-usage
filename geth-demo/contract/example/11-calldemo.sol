66b136909f9b95046151af5bb0e98edae07cf328
vsice33 <19449477+vsice33@users.noreply.github.com>
2022-02-15 15:08:29 +0800

    add calldemo contract.

---
+++ geth-demo/contract/example/11-calldemo.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity^0.8.7;
+
+contract calldemo {
+    uint256 count;
+
+    constructor() {
+        count = 2020;
+    }
+
+    function setCount(uint256 _count) external {
+        count = _count;
+    }
+
+    function getCount() public view returns(uint256) {
+        return count;
+    }
+}
